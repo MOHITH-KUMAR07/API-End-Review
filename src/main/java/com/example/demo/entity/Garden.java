c8351a638bb5559a6b336df0b99878870a60549a
MOHITH-KUMAR07 <727723eucs124@skcet.ac.in>
2025-04-03 22:16:33 +0530

    code updated

--- src/main/java/com/example/demo/entity/Garden.java
+++ src/main/java/com/example/demo/entity/Garden.java
@@ -1,5 +1,24 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Garden {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "details_id", referencedColumnName = "id")
+    private GardenDetails gardenDetails;
 }
